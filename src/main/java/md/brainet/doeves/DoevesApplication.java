7ccf3281761ae6b526f0a5a98d795753745ab27f
Igor <lovespringmvc@gmail.com>
2024-05-20 15:39:58 +0300

    Init Spring Boot project

---
+++ src/main/java/md/brainet/doeves/DoevesApplication.java
@@ -0,0 +1,13 @@
+package md.brainet.doeves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoevesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoevesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/md/brainet/doeves/DoevesApplicationTests.java
@@ -0,0 +1,13 @@
+package md.brainet.doeves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoevesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
